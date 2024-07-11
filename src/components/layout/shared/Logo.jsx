e05ed3fab9d28151649453f4f71bd6e87db98767
Gesang Paudra J <g.paudra@gmail.com>
2024-07-11 19:49:44 +0700

    Update Logo.jsx

--- src/components/layout/shared/Logo.jsx
+++ src/components/layout/shared/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = ({ color }) => {
   return (
     <div className='flex items-center min-bs-[24px]'>
       <i className="ri-receipt-fill"></i>
-      <h1 className='text-lg pl-[10px]'>KASBON MANAGER</h1>
+      <h1 className='text-lg pl-[10px]'>DEBT MANAGER</h1>
     </div>
   )
 }
